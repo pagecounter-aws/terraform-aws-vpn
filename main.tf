d6df2ee63d59728b1f930c14c17399a66b0ded2f
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-09-10 19:27:48 +0200

    update

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
-
 data "aws_vpc" "selected" {
   id = var.aws_vpc_id
 }
 
-resource "aws_vpn_gateway" "aws-vpn-gw" {
+resource "aws_vpn_gateway" "vpn-gw" {
   vpc_id = var.aws_vpc_id
 }
 
-resource "aws_customer_gateway" "aws-cgw" {
+resource "aws_customer_gateway" "cgw" {
   bgp_asn    = var.bgp_asn
   ip_address = var.customer_gateway
   type       = "ipsec.1"
@@ -18,13 +17,13 @@ resource "aws_customer_gateway" "aws-cgw" {
 }
 
 resource "aws_vpn_gateway_route_propagation" "route_propagation" {
-  vpn_gateway_id = aws_vpn_gateway.aws-vpn-gw.id
+  vpn_gateway_id = aws_vpn_gateway.vpn-gw.id
   route_table_id = data.aws_vpc.selected.main_route_table_id
 }
 
 resource "aws_vpn_connection" "aws-vpn-connection" {
-  vpn_gateway_id      = aws_vpn_gateway.aws-vpn-gw.id
-  customer_gateway_id = aws_customer_gateway.aws-cgw.id
+  vpn_gateway_id      = aws_vpn_gateway.vpn-gw.id
+  customer_gateway_id = aws_customer_gateway.cgw.id
   type                = "ipsec.1"
   static_routes_only  = false
 
@@ -32,18 +31,3 @@ resource "aws_vpn_connection" "aws-vpn-connection" {
     "Name" = var.aws-vpn-connection
   }
 }
-
-# Allow traffic from the VPN subnets.
-resource "aws_security_group" "aws-allow-vpn" {
-  name        = "aws-allow-vpn"
-  description = "Allow all traffic from vpn resources"
-  vpc_id      = var.aws_vpc_id
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,3 @@
-
-output "tunnel1_address" {
-  value = aws_vpn_connection.aws-vpn-connection.tunnel1_address
-}
-
-output "tunnel1_preshared_key" {
-  value = aws_vpn_connection.aws-vpn-connection.tunnel1_preshared_key
-}
-
-output "tunnel2_address" {
-  value = aws_vpn_connection.aws-vpn-connection.tunnel2_address
-}
-
-output "tunnel2_preshared_key" {
-  value = aws_vpn_connection.aws-vpn-connection.tunnel2_preshared_key
-}
-
-output "all" {
-  value = aws_vpn_connection.aws-vpn-connection
+output "customer_gateway_configuration" {
+  value = aws_vpn_connection.aws-vpn-connection["customer_gateway_configuration"]
 }
